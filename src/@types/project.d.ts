616de1158d4a35609eb996985d954c040b584bd4
gouteuxchristophe <chris.gouteux@outlook.fr>
2023-05-03 13:51:41 +0200

    sprint1/dev/add project interface

---
+++ src/@types/project.d.ts
@@ -0,0 +1,27 @@
+export interface Project {
+  id: number
+  title: string
+  author: IAuthor
+  content: string
+  member_projet: IMemberProjet[]
+  techno_projet: ITechnoProjet[]
+  created_at: string
+  updated_at?: string
+  status: string
+}
+
+export interface IAuthor {
+  id: number
+  pseudo: string
+}
+
+export interface IMemberProjet {
+  id: number
+  pseudo: string
+}
+
+export interface ITechnoProjet {
+  id: number
+  label: string
+  color?: string
+}
