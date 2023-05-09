281b61af3bfff82a586f64709430d82f3dd77c2b
gouteuxchristophe <chris.gouteux@outlook.fr>
2023-05-09 11:15:23 +0200

    [data] Techno - Add techno json

---
+++ src/store/techno.js
@@ -0,0 +1,37 @@
+const data = [
+  {
+    id: 6,
+    label: 'Angular',
+    color: 'D2002F',
+    created_at: '2023-05-09T07:19:18.745Z',
+    updated_at: null,
+    delete_at: null,
+  },
+  {
+    id: 7,
+    label: 'Docker',
+    color: '0069AD',
+    created_at: '2023-05-09T07:19:24.999Z',
+    updated_at: null,
+    delete_at: null,
+  },
+  {
+    id: 8,
+    label: 'HTML',
+    color: 'D84924',
+    created_at: '2023-05-09T07:19:32.595Z',
+    updated_at: null,
+    delete_at: null,
+  },
+  {
+    id: 9,
+    label: 'CSS',
+    color: 'F2F2F2',
+    created_at: '2023-05-09T07:19:38.054Z',
+    updated_at: null,
+    delete_at: null,
+  },
+
+];
+
+export default data;
