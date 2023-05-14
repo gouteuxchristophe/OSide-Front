e922585ac626d9b8571864d0436d7489dd633ca3
ThomasBiget <t.biget@gmail.com>
2023-05-14 22:50:33 +0200

    [JS]add comments in code

--- src/components/Register/index.tsx
+++ src/components/Register/index.tsx
@@ -1,7 +1,7 @@
 import { ChangeEvent, FormEvent, useState } from 'react';
 import axiosInstance from 'axios';
 import { Eye, EyeOff } from 'react-feather';
-
+// typage des éléments envoyés en post à l'API
 interface InputProps {
   user_name: string,
   last_name: string,
@@ -12,23 +12,43 @@ interface InputProps {
 }
 
 function Register() {
+  // state qui va contenir tout le contenu des inputs au fur
+  // et à mesure qu'ils se remplissent par le user
   const [inputs, setInputs] = useState<InputProps>({});
+  // state qui sera un booléen qui va permettre de valider
+  // que le password et la confirmation de password sont identiques
+  // au moment du la soumission du form (voir dans handleSubmitForm)
   const [passwordVerification, setPasswordVerification] = useState(false);
+  // les deux states ci-dessous sont des booléens
+  // qui permettent d'afficher ou de masquer les passwords en cliquant sur l'icone 'oeil'
+  // (voir dans handleEyeClickPwd et handleEyeClickPwdVerif)
   const [clickEye, setClickEye] = useState(false);
   const [clickEyeVerif, setClickEyeVerif] = useState(false);
+  // state booléen qui va permettre la vérification du format de l'email
   const [emailVerification, setEmailVerification] = useState(false);
 
+  // handleChange permet de récupérer les contenus des inputs (onChange)
+  //  et de l'ajouter au state 'inputs' au fur et à mesure
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     const { name } = e.target;
     const { value } = e.target;
     setInputs((values) => ({ ...values, [name]: value }));
   };
+  // permet de vérifier le format de l'email
   const isValidEmail = (email: string) => /\S+@\S+\.\S+/.test(email);
 
+  // permet d'envoyer les éléments contenus dans les inputs (en donc dans le state 'inputs')
+  // tout en vérifiant au moment du submit le format de l'email
+  // et la correspondance des deux passwords
   const handleSubmitForm = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    // on reset ces deux states afin de faire disparaître les messages
+    // d'erreur s'ils sont affichés ('Les passwords ne correspondent pas'
+    // et 'l'email n'est pas valide')
     setPasswordVerification(false);
     setEmailVerification(false);
+    // si les deux passords et le format de l'email sont bon
+    // on envoie un post vers l'API
     if (inputs.user_password as string === inputs.user_password_confirmation
       && isValidEmail(inputs.user_email)) {
       console.log(inputs);
@@ -39,18 +59,21 @@ function Register() {
         .catch((error) => {
           console.log(error);
         });
+      // sinon on affiche les message que les deux passwords ne correspondent pas
     } else if (inputs.user_password as string !== inputs.user_password_confirmation) {
       setPasswordVerification(true);
+    // et si ce n'est pas les passwords cela signifie que le format de l'email n'est pas correct
     } else {
       setEmailVerification(true);
     }
   };
-
+  // écouteur sur l'icone oeil du password pour l'afficher
   const handleEyeClickPwd = () => {
     if (clickEye === false) {
       setClickEye(true);
     } else if (clickEye === true) { setClickEye(false); }
   };
+  // écouteur sur l'icone oeil du password vérification pour l'afficher
   const handleEyeClickPwdVerif = () => {
     if (clickEyeVerif === false) {
       setClickEyeVerif(true);
