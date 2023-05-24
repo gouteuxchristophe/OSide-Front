bd0c6bfc70f5f9ab235cb9142abe9e9ef743f47c
Matthieu LP <lepriol.matthieu@gmail.com>
2023-05-24 22:01:17 +0200

    Update login with Github & link footer

--- src/components/App/Footer/index.tsx
+++ src/components/App/Footer/index.tsx
@@ -5,11 +5,9 @@ function Footer() {
     <div className="flex flex-col justify-around items-center mt-2 h-full ">
       <p className="tracking-wider text-center ">O&apos;Side copyright 2023</p>
       <p className='tracking-wider '>Créer par trois étudiants 
-      <a className='text-fof font-bold' target='_blank' href="https://oclock.io/?utm_source=google&utm_medium=cpc&utm_campaign=brand&adgroupname=oclock&keyword=o%27clock&gad=1&gclid=Cj0KCQjwyLGjBhDKARIsAFRNgW_ppkEcl9Cul7Y6nmJtgccpR1Jil1VTFFWdjkKS025ituQNlkEhK4MaAsYSEALw_wcB">O'Clock</a>
+      <a className='text-fof font-bold' target='_blank' href="https://oclock.io/">O'Clock</a>
       </p>
-      <Link to="/swagger-ui" className="tracking-wider text-center">
-        Swagger
-      </Link>
+      <a href="https://oside.mimouss.fr/swagger-ui/" className='tracking-wider text-center' target='_blank' >Swagger</a>
       <div className="flex justify-center gap-2 items-center p-2">
         <Link to="/about" className="p-2 border border-solid border-[white] rounded bg-primary0 tracking-wider">
           About
