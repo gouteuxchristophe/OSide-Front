978c043f07e72157f38b7ba74ff49458d53d08eb
gouteuxchristophe <chris.gouteux@outlook.fr>
2023-05-24 09:41:53 +0200

    Co-authored-by: Thomas BIGET <ThomasBiget@users.noreply.github.com> Co-authored-by: Mouss <Mimouss56@users.noreply.github.com> Co-authored-by: Mouss <lepriol.matthieu@gmail.com>

--- src/components/App/Footer/index.tsx
+++ src/components/App/Footer/index.tsx
@@ -4,7 +4,12 @@ function Footer() {
   return (
     <div className="flex flex-col justify-around items-center mt-2  ">
       <p className="tracking-wider text-center ">O&apos;Side copyright 2023</p>
-      <p className='tracking-wider '>Créer par trois étudiants <a className='text-fof font-bold' target='_blank' href="https://oclock.io/?utm_source=google&utm_medium=cpc&utm_campaign=brand&adgroupname=oclock&keyword=o%27clock&gad=1&gclid=Cj0KCQjwyLGjBhDKARIsAFRNgW_ppkEcl9Cul7Y6nmJtgccpR1Jil1VTFFWdjkKS025ituQNlkEhK4MaAsYSEALw_wcB">O'Clock</a></p>
+      <p className='tracking-wider '>Créer par trois étudiants 
+      <a className='text-fof font-bold' target='_blank' href="https://oclock.io/?utm_source=google&utm_medium=cpc&utm_campaign=brand&adgroupname=oclock&keyword=o%27clock&gad=1&gclid=Cj0KCQjwyLGjBhDKARIsAFRNgW_ppkEcl9Cul7Y6nmJtgccpR1Jil1VTFFWdjkKS025ituQNlkEhK4MaAsYSEALw_wcB">O'Clock</a>
+      </p>
+      <Link to="/swagger-ui" className="tracking-wider text-center">
+        Swagger
+      </Link>
       <div className="flex justify-center gap-2 items-center p-2">
         <Link to="/about" className="p-2 border border-solid border-[white] rounded bg-primary0 tracking-wider">
           About
